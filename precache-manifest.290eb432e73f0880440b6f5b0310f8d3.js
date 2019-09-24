f3593b1d442d6f06aa754860d9a76745c35eb0b6
Seth Martin <sethmartin@Seths-MacBook-Pro.local>
2019-09-23 22:50:43 -0700

    Updates

---
+++ precache-manifest.290eb432e73f0880440b6f5b0310f8d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c786ec4e892b8f35a9ae6b8a1bb01fa7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5eb34c770b36e2b8aa09",
+    "url": "/monsters-rolodex/static/css/main.1c8472f2.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "5eb34c770b36e2b8aa09",
+    "url": "/monsters-rolodex/static/js/main.d5cc5bfc.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
